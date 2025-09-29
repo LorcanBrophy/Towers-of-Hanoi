625b450d037e6bf6f18cd957643822b6e411fc24
LorcanBrophy <20112489@mail.wit.ie>
2025-09-29 02:55:46 +0100

    Rectangle can be properly moved with mouse

--- src/main/java/com/example/gui_practice/Controller.java
+++ src/main/java/com/example/gui_practice/Controller.java
@@ -8,26 +8,8 @@ import javafx.scene.input.MouseEvent;
 
 
 public class Controller {
-    @FXML private Pane tower1, tower2, tower3;
-    @FXML private Rectangle base1, base2, base3;
-    @FXML private Rectangle disc1, disc2, disc3;
+    @FXML private Rectangle rect1;
 
-    @FXML
-    public void initialize() {
-        setupBase(base1, tower1);
-        setupBase(base2, tower2);
-        setupBase(base3, tower3);
-    }
-
-    private void setupBase(Rectangle base, Pane tower) {
-        base.widthProperty().bind(tower.widthProperty());
-        base.layoutYProperty().bind(tower.heightProperty().subtract(base.heightProperty()));
-    }
-
-
-    private void setupDisc() {}
-
-    /*
     private double offsetX;
     private double offsetY;
 
@@ -41,5 +23,5 @@ public class Controller {
     public void mouseDragged(MouseEvent mouseEvent) {
         rect1.setLayoutX(mouseEvent.getSceneX() - offsetX);
         rect1.setLayoutY(mouseEvent.getSceneY() - offsetY);
-    }*/
+    }
 }
