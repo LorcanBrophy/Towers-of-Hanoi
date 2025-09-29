20ae3921c66840705b48efe75e6aabd03bb58fa5
LorcanBrophy <20112489@mail.wit.ie>
2025-09-29 03:07:16 +0100

    Added snapping of discs using "onMouseReleased" and checking which third of the screen the disc was in.

--- src/main/java/com/example/gui_practice/Controller.java
+++ src/main/java/com/example/gui_practice/Controller.java
@@ -1,27 +1,40 @@
 package com.example.gui_practice;
 
 import javafx.fxml.FXML;
-import javafx.scene.layout.Pane;
+
 import javafx.scene.shape.Rectangle;
 
 import javafx.scene.input.MouseEvent;
 
 
 public class Controller {
-    @FXML private Rectangle rect1;
+    @FXML private Rectangle disc1;
 
     private double offsetX;
     private double offsetY;
 
     @FXML
     public void mousePressed(MouseEvent mouseEvent) {
-        offsetX = mouseEvent.getSceneX() - rect1.getLayoutX();
-        offsetY = mouseEvent.getSceneY() - rect1.getLayoutY();
+        offsetX = mouseEvent.getSceneX() - disc1.getLayoutX();
+        offsetY = mouseEvent.getSceneY() - disc1.getLayoutY();
     }
 
     @FXML
     public void mouseDragged(MouseEvent mouseEvent) {
-        rect1.setLayoutX(mouseEvent.getSceneX() - offsetX);
-        rect1.setLayoutY(mouseEvent.getSceneY() - offsetY);
+        disc1.setLayoutX(mouseEvent.getSceneX() - offsetX);
+        disc1.setLayoutY(mouseEvent.getSceneY() - offsetY);
+    }
+    // I can make it so you check the nearest disc and move it into the right position, rather than making specific conditions for each disc
+    public void mouseReleased() {
+        if (disc1.getLayoutX() < 300) {
+            disc1.setLayoutX(100);
+            disc1.setLayoutY(500);
+        } else if (disc1.getLayoutX() < 600) {
+            disc1.setLayoutX(400);
+            disc1.setLayoutY(500);
+        } else {
+            disc1.setLayoutX(700);
+            disc1.setLayoutY(500);
+        }
     }
 }
